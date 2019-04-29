3368cd0540d938971335093f30c0d753e74a0836
Gabriel Tadeo del Campo Aceves <gtdca98@gmail.com>
2019-04-29 16:53:38 -0500

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,121 @@
 
+
+
+var cProd = [
+['C. solo gris azul agua.jpg' ,   0],
+['C. solo menta  amarillo.jpg',   0]
+]
+var cProdbis = []
+
+for (var rr = 0; rr < cProd.length   ; rr ++){
+    cProdbis[rr] = '<td><a href="Col_FyF/c/' + cProd[rr][0] + '"><img class="d-block w30" src="Col_FyF/c/s' + cProd[rr][0] + '" ><p class= "tcn">$ '+ cProd[rr][1] +' </p></a></td>'
+}  
+
+
+
+
+//proceso de pulseras solas
+
+var pProd = [
+["P sola roja.jpg"         ,   0],
+["P sola rosa agua.jpg"    ,   0],
+["P sola rosa negro.jpg"    ,   0],
+["P sola verde gris.jpg"    ,   0],
+["P. sola azul cafe.jpg"    ,   0],
+["P. sola cafe broche.jpg"  ,   0],
+["P. sola menta agua.jpg"   ,   0],
+["P. sola rosa agua.jpg"    ,   0],
+["P. sola violeta.jpg"      ,   0]
+]
+var pProdbis = []
+
+for (var rr = 0; rr < pProd.length   ; rr ++){
+    pProdbis[rr] = '<td><a href="Col_FyF/p/' + pProd[rr][0] + '"><img class="d-block w30" src="Col_FyF/p/s' + pProd[rr][0] + '" ><p class= "tcn">$ '+ pProd[rr][1] +' </p></a></td>'
+}  
+
+
+//proceso de taloneras
+var tProd = [
+["Talonera atrapasueños.jpg"    ,   0],
+["Talonera caracol.jpg"         ,   0],
+["Talonera cola de ballena.jpg" ,   0],
+["Talonera cuadro azul.jpg"     ,   0],
+["Talonera pompon morado.jpg"   ,   0]
+];
+var tProdbis = []
+for (var rr = 0; rr < tProd.length   ; rr ++){
+    tProdbis[rr] = '<td><a href="Col_FyF/t/' + tProd[rr][0] + '"><img class="d-block w30" src="Col_FyF/t/s' + tProd[rr][0] + '" ><p class= "tcn">$ '+ tProd[rr][1] +' </p></a></td>'
+}  
+
+
+function generaCarruselo(caption,nombre, arrayProdBis,objetocontenedor){''
+	var tcaritem = '<p class="title" >Mexicana Mía</p>' + '<p class="title">'+ caption+' </p>' + '<div id="'+nombre+'" class="carousel slide" data-ride="carousel" data-interval="10000">';
+    tcaritem = tcaritem + '<div class="carousel-inner">'
+
+	var salidai = "";
+	var conseci =0
+	for (var tt = 0; tt < Math.ceil(arrayProdBis.length/3)   ; tt ++){
+		salidai =  '<div class="carousel-item' + ( tt==0 ?  ' active ">'  : '">' ) + '<table><tr>' + '<td width="35%"></td>' ;
+		for (var ii =0; ii < 3; ii ++){
+			if (conseci < arrayProdBis.length){
+				salidai = salidai + ' '	+ arrayProdBis[conseci]
+				conseci ++
+			}
+		}
+	   salidai = salidai +  '<td width="35%"></td>' +'</tr></table>' + '</div>';
+	   tcaritem = tcaritem + salidai	
+	}
+    tcaritem = tcaritem + '</div>' + '<a class="carousel-control-prev" href="#'+nombre+'" role="button" data-slide="prev">'
+    tcaritem = tcaritem + '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+'<span class="sr-only">Previous</span>'+ '</a>'
+    tcaritem = tcaritem + '<a class="carousel-control-next" href="#'+nombre+'" role="button" data-slide="next">'
+    tcaritem = tcaritem + '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+ '<span class="sr-only">Next</span>'+'</a>'+'</div>'+ '<hr>'
+
+    $('#' + objetocontenedor).append(tcaritem);
+
+}
+
+
+
+
+//variables para el carrusel
+
+var lcarA = '<div id="tresProductos" class="carousel slide" data-ride="carousel">'
+lcarA= lcarA + '<div class="carousel-inner">'
+lcarA= lcarA + '    <div class="carousel-item ">' //active
+lcarA= lcarA + '      <table><tr>'
+lcarA= lcarA + '      	<td width="35%"></td>'
+lcarA= lcarA + '      	<td><a href="Col_FyF/'
+var lcar2=     '"><img class="d-block w30" src="Col_FyF/'
+var lcar3=     '" ><p class= "tcn">$ 135</p></a></td>'
+var lcarB =   '</tr></table>' + '</div>'  //cierra div carrousel-item
+
+
+
+/*
+var lcarB =  lcarB +      '    <div class="carousel-item">'
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#tresProductos" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#tresProductos" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+*/
+
+
+
+
+
+
+
 var lits= [ 
 ["flor blanca de cactus.jpg" ,"Colección Flores y Fachadas","A.flor blanca de cactus.jpg",125,"SI","P flor blanca de cactus.jpg",140,"SI","C.flor blanca de cactus.jpg",240,"SI",505],
 ["F.rosa y menta.JPG"        ,"Colección Flores y Fachadas","none.jpg"                    , 0,"NO","P.F rosa y menta.jpg"       ,160,"SI","C.F.rosa y menta.jpg"       ,260,"SI",420],
